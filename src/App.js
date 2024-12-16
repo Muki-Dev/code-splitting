ad45802585010bd51a0112c550f4713be4da455f
Muki Dev <mukidev881@gmail.com>
2024-12-16 16:31:13 +0000

    code splitting with import syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Page3 from './components/Page3';
+// import Page2 from './components/Page2';
+// import Page3 from './components/Page3';
 import './App.css';
 
 class App extends Component {
@@ -15,17 +15,37 @@ class App extends Component {
 
     onRouteChange = (route) => {
       // No Code Splitting
-      this.setState({route: route})
+      // this.setState({route: route})
+
+      // With Code Splitting
+      if(route === 'page1'){
+        this.setState({route: route})
+      }else if(route === 'page2'){
+        import('./components/Page2').then((Page2) => {
+          this.setState({route: route, components: Page2.default})
+        })
+      }else if(route === 'page3'){
+        import('./components/Page3').then((Page3) => {
+          this.setState({route: route, components: Page3.default})
+        })
+      }
     }
   render(){
     // No Code Splitting
-      if(this.state.route ==='page1'){
-          return <Page1 onRouteChange={this.onRouteChange}/>
-      }else if(this.state.route ==='page2'){
-         return <Page2 onRouteChange={this.onRouteChange}/>
-       }else if(this.state.route ==='page3'){
-           return <Page3 onRouteChange={this.onRouteChange}/>
-       }
+      // if(this.state.route ==='page1'){
+      //     return <Page1 onRouteChange={this.onRouteChange}/>
+      // }else if(this.state.route ==='page2'){
+      //    return <Page2 onRouteChange={this.onRouteChange}/>
+      //  }else if(this.state.route ==='page3'){
+      //      return <Page3 onRouteChange={this.onRouteChange}/>
+      //  }
+
+    // With Code Splitting
+    if(this.state.route){
+      return <Page1 onRouteChange={this.onRouteChange} />
+    }else{
+      return <this.state.components onRouteChange={this.onRouteChange} />
+    }
   }
   
 }
